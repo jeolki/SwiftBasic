1142bb51463c915e3a01cf84f08e8eca4f153365
hongjeonggi <hongjeonggi35@gmail.com>
2021-12-29 23:57:22 +0900

    Update optional description

--- contents/01_Basic/10_optional/optional_unwrapping.swift
+++ contents/01_Basic/10_optional/optional_unwrapping.swift
@@ -1,11 +1,31 @@
 /* 옵셔널 추출 */
 
-// 옵셔널 바인딩
-// 강제 추출
+/*
+    열거형의 some 케이스로 꼭꼭 숨어있는 옵셔널의 값을 옵셔널이 아닌 값으로 추출하는
+    옵셔널 추출 방법에 대해 알아보겠습니다.
+*/
 
 import Swift
 
-//MARK:- Optional Binding
+ 
+//MARK: 강제 추출
+/*
+    - 옵셔널 강제 추출 방식은 옵셔널의 값을 추출하는 가장 간단하지만 가장 위험한 방법입니다.
+    - 런타입 오류가 일어날 가능성이 가장 높기 때문입니다
+    - 또, 옵셔널을 만든 의미가 무색해지는 방법이기도 합니다.
+    - 옵셔널의 값을 강제 추출하려면 옵셔널 값의 뒤에 느낌표(!)를 붙여주면 값을 강제로 추출하여 반환해줍니다.
+    - 만약 강제 추출 시 옵셔널에 값이 없다면, 즉 nil이라면 런타임 오류가 발생합니다.
+*/
+
+
+//MARK: Optional Binding
+
+/*
+    - 옵셔널 바인딩은 옵셔널에 값이 있는지 확인할 때 사용합니다.
+    - 만약 옵셔널에 값이 있다면 옵셔널에서 추출한 값을 일정 블록 안에서 사용할 수 있는 상수나 변수로
+      할당해서 옵셔널이 아닌 형태로 사용할 수 있도록 해줍니다.
+    - 옵셔널 바인딩은 if 또는 while 구문 등과 결합하여 사용할 수 있습니다.
+*/
 
 func printName(_ name: String) {
     print(name)
@@ -37,7 +57,7 @@ if let name: String = yourName {
 
 // ,를 사용해 한 번에 여러 옵셔널을 바인딩 할 수 있습니다
 // 모든 옵셔널에 값이 있을 때만 동작합니다
-myName = "yagom"
+myName = "willer"
 yourName = nil
 
 if let name = myName, let friend = yourName {
@@ -53,7 +73,7 @@ if let name = myName, let friend = yourName {
 // yagom and hana
 
 
-//MARK:- Force Unwrapping
+//MARK: Force Unwrapping
 
 printName(myName!) // yagom
 
